8cac2b6852bf4953a5638bd7f26283ddf00924d1
bakterio <lukastoman594@gmail.com>
2022-04-02 10:17:23 +0200

    ServerCommand

--- src/main/java/cz/bakterio/discord/Tux/commands/supercommands/ServerCommand.java
+++ src/main/java/cz/bakterio/discord/Tux/commands/supercommands/ServerCommand.java
@@ -1,17 +1,18 @@
 package cz.bakterio.discord.Tux.commands.supercommands;
 
 import cz.bakterio.discord.Tux.commands.Command;
-import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.awt.*;
 import java.util.List;
 
 public class ServerCommand extends Command {
     @Override
     public String getName() {
-        return "server";
+        return "servers";
     }
 
     @Override
@@ -31,9 +32,11 @@ public class ServerCommand extends Command {
 
     @Override
     public void invoke(GuildMessageReceivedEvent e, String[] args) {
-        final MessageBuilder builder = new MessageBuilder("I am on these servers:\n");
+        final EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle("Servers");
+        builder.setColor(Color.YELLOW);
         for (Guild g : e.getJDA().getGuilds()) {
-            builder.append(g.getName() + "\n");
+            builder.addField(g.getName() + "\n", "Members: " + g.getMemberCount(), false);
         }
         e.getChannel().sendMessage(builder.build()).queue();
     }
